5c78fed11e662978e1970b99a0acfb3ffb002f5f
Yashar Imanlou <yashari@gmail.com>
2023-12-01 14:31:59 +0100

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,16 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.sumOf { "${it.firstDigitChar()}${it.reversed().firstDigitChar()}".toInt() }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.map(String::filterDigits)
+            .sumOf { "${it.firstDigitChar()}${it.reversed().firstDigitChar()}".toInt() }
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(readInput("Day01_test_1")) == 142)
+    check(part2(readInput("Day01_test_2")) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
--- src/Utils.kt
+++ src/Utils.kt
@@ -3,6 +3,19 @@ import java.security.MessageDigest
 import kotlin.io.path.Path
 import kotlin.io.path.readLines
 
+val digitStrings = listOf(
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+)
+
 /**
  * Reads lines from the given input txt file.
  */
@@ -14,6 +27,19 @@ fun readInput(name: String) = Path("src/$name.txt").readLines()
 fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
     .toString(16)
     .padStart(32, '0')
+
+fun String.firstDigitChar(): Char = this.toCharArray().first { it.isDigit() }
+
+fun String.filterDigits(): String {
+    val digits = StringBuilder()
+
+    for (i in indices) {
+        if (this[i].isDigit()) digits.append(this[i])
+        digitStrings.forEachIndexed { digit, str -> if (indexOf(str, i) == i) digits.append(digit) }
+    }
+
+    return digits.toString()
+}
 
 /**
  * The cleaner shorthand for printing output.
