48945c9fea14e24786540e011e3f7951c722432d
Yashar Imanlou <yashari@gmail.com>
2023-12-03 18:56:03 +0400

    Move declarations into the main scope

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,4 +1,31 @@
 fun main() {
+    data class Colors(var r: Int = 0, var b: Int = 0, var g: Int = 0)
+
+    fun inBoundary(pair: Pair<Int, Colors>): Boolean = pair.second.run { return r <= 12 && b <= 14 && g <= 13 }
+
+    fun toPower(pair: Pair<Int, Colors>): Int = pair.second.run { return r * b * g }
+
+    fun parseLine(line: String): Pair<Int, Colors> {
+        val colors = Colors()
+        val l = line.lowercase()
+        val splits = l.split(":")
+        val id = splits[0].replaceFirst("game", "").trim().toInt()
+
+        splits[1].trim().replace(";", ",").split(",").forEach { comb ->
+            val c = comb.trim().split(" ")
+            val num = c[0]
+            val color = c[1]
+
+            when (color) {
+                "red" -> if(num.toInt() > colors.r) colors.r = num.toInt()
+                "blue" -> if(num.toInt() > colors.b) colors.b = num.toInt()
+                "green" -> if(num.toInt() > colors.g) colors.g = num.toInt()
+            }
+        }
+
+        return id to colors
+    }
+
     fun part1(input: List<String>): Int {
         return input.map { parseLine(it) }.filter { inBoundary(it) }.sumOf { it.first }
     }
@@ -15,30 +42,3 @@ fun main() {
     part1(input).println()
     part2(input).println()
 }
-
-fun inBoundary(pair: Pair<Int, Colors>): Boolean = pair.second.run { return r <= 12 && b <= 14 && g <= 13 }
-
-fun toPower(pair: Pair<Int, Colors>): Int = pair.second.run { return r * b * g }
-
-fun parseLine(line: String): Pair<Int, Colors> {
-    val colors = Colors()
-    val l = line.lowercase()
-    val splits = l.split(":")
-    val id = splits[0].replaceFirst("game", "").trim().toInt()
-
-    splits[1].trim().replace(";", ",").split(",").forEach { comb ->
-        val c = comb.trim().split(" ")
-        val num = c[0]
-        val color = c[1]
-
-        when (color) {
-            "red" -> if(num.toInt() > colors.r) colors.r = num.toInt()
-            "blue" -> if(num.toInt() > colors.b) colors.b = num.toInt()
-            "green" -> if(num.toInt() > colors.g) colors.g = num.toInt()
-        }
-    }
-
-    return id to colors
-}
-
-data class Colors(var r: Int = 0, var b: Int = 0, var g: Int = 0)
